2054bc8d18b72b5870560740ba640f504a509e0b
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-15 12:33:26 +0100

    [Routes]; add: Enable routing and nexted routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import TodoContainer from './components/TodoContainer';
+import About from './components/pages/About';
+import NotMatch from './components/pages/NotMatch';
+import AboutPages from './components/pages/AboutPages';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />}>
+          <Route path=":aboutPage" element={<AboutPages />} />
+        </Route>
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
